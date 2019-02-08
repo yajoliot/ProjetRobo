fac58c92f56e1b1b88ff3f46c3de7643b30890c1
Yan Joliot <yajola@l3818-03.info.polymtl.ca>
2019-02-08 15:19:01 -0500

    tp5 debut

---
+++ TP5-PR1/memoire_24.d
@@ -0,0 +1,1 @@
+memoire_24.o: memoire_24.cpp memoire_24.h
--- TP5-PR1/pr1.cpp
+++ TP5-PR1/pr1.cpp
@@ -7,6 +7,9 @@
 #include <util/delay.h>
 #include <avr/interrupt.h>
 
+
+#include "memoire_24.h"
+
 const int ETEIND = 0; //DEL ETEINTE
 const int ROUGE = 1; //DEL ROUGE
 const int VERT = 2; //DEL VERTE
@@ -15,54 +18,37 @@ enum etats {INIT,E1,E2,E3,E4,S};
 volatile int etat = INIT;
 
 
-void isr_INIT() {
 
-    DDRD = 0x00;
-    DDRB = 0xff;
-
-    
-    cli ();
-
-    EIMSK |= (1 << INT0);
-
-    EICRA |= (1 << ISC01); // EICRA = EICRA | (1 << ISC01)
-
-
-    sei ();
-
-
-}
-
-ISR(INT0_vect){
-
-
-   _delay_ms(30);
-
-    EIFR |= (1 << INTF0);
-    PORTB = VERT;
-}
 
 int main(){
 
-    isr_INIT();
+    DDRB = 0xff;
+    uint16_t adresse = 0x00;
+    uint8_t tableauPoly[] = {'*','P','*','O','*','L','*','Y','*','T','*','E','*','C','*','H','*','N','*','I','*','Q','*','U','*','E','*',' ','*','M','*','O','*','N','*','T','*','R','*','E','*','A','*','L','*'};
+    uint8_t taille = sizeof(tableauPoly);
+    uint8_t lecture[46] = {};
 
 
-    _delay_ms(50000);
-
-    PORTB = ROUGE;
-    _delay_ms(1000);
-    PORTB = ETEIND;
+    Memoire24CXXX memoire = Memoire24CXXX();
+    //memoire.init();
+    memoire.ecriture(adresse, tableauPoly, taille);
+    _delay_ms(10);
+    memoire.lecture(adresse, lecture, taille);
     
-    _delay_ms(10000);
+    bool bon = false;
 
-    cli();
+    for(int i = 0; i < sizeof(tableauPoly); i++){
+        if(tableauPoly[i] == lecture[i]){
+            PORTB = VERT;
+        }else{
+            PORTB = ROUGE;
+        }
 
-    if(PORTB != VERT)
-        PORTB = ROUGE;
-    
+        if(PORTB == ROUGE){
+            break;
+        }
 
-    
-    
+    }
 
 
    return 0;
