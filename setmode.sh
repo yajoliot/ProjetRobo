2f1cb38f16be113d9a247018d04ebb988b5f12af
Simon Zhang <babysheebo@gmail.com>
2019-03-13 15:53:03 -0400

    correct setmode.sh behaviour

--- setmode.sh
+++ setmode.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 source variables
+echo $CURRENT_DIR
 if [ $1 == "release" ]
 	then
-		awk '{gsub(/DEBUG_CMD=.*/,"DEBUG_CMD=")}' $CURRENT_DIR/variables.mk > $CURRENT_DIR/variables.mk
+		mv $CURRENT_DIR/variables.mk variables.mk.old && awk '{gsub(/DEBUG_CMD=.*/,"DEBUG_CMD=")}1' variables.mk.old > $CURRENT_DIR/variables.mk && rm variables.mk.old
 elif [ $1 == "debug" ]
-	then 
-		awk '{gsub(/DEBUG_CMD=.*/,"DEBUG_CMD=DEBUG")}' $CURRENT_DIR/variables.mk > $CURRENT_DIR/variables.mk
+	then
+		mv $CURRENT_DIR/variables.mk variables.mk.old && awk '{gsub(/DEBUG_CMD=.*/,"DEBUG_CMD=DEBUG")}1' variables.mk.old > $CURRENT_DIR/variables.mk && rm variables.mk.old
 else
 	echo "Unknown parameter"
+fi
