74cc25b7fcb32bf0baca0492971e2ee98422095f
Simon Zhang <simon.zhang@polymtl.ca>
2019-03-15 19:32:48 -0400

    add lookup table of ICRn values

--- codeCommun/tp/tp9/exec_dir/main.cpp
+++ codeCommun/tp/tp9/exec_dir/main.cpp
@@ -1,20 +1,24 @@
 #include <avr/io.h>
 #include "couleurDel.h"
+#include "notes.h"
 
 int main() {
     DDRD |= _BV(DDD4); //OC1B as output
     TCCR1A |= _BV(COM1B1);  //Clear OC1A/OC1B on compare match
 	TCCR1B |= _BV(WGM13) 	//mode 8, PWM, Phase and Frequency Correct (TOP value is ICR1)
    		   |  _BV(CS11); 	//prescaler(8)
-   	const uint16_t note[] = { 1911, 1703, 1517 , 1432 , 1276 , 1136 , 1012 };
+   	// const uint16_t note[] = { 1911, 1703, 1517 , 1432 , 1276 , 1136 , 1012 };
    	OCR1B = 100;
-   	uint8_t count = 0;
+   	uint8_t count = 0//56;
    	for(;;){
 		ICR1H = (note[count] >> 8); //first set the high byte
 		ICR1L = note[count];        //now the low byte
 		_delay_ms(2000);
 		count++;
-		count=count%7;
+		count=count%116;
+		// if(count==0){
+			// count=56;
+		// }
 	}
     return 0;
 }
