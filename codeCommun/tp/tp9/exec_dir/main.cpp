e48736dbde45df2592c2289e75a1c3cf1b273429
Simon Zhang <simon.zhang@polymtl.ca>
2019-03-15 17:22:08 -0400

    add example of working midi code

--- codeCommun/tp/tp9/exec_dir/main.cpp
+++ codeCommun/tp/tp9/exec_dir/main.cpp
@@ -2,10 +2,19 @@
 #include "couleurDel.h"
 
 int main() {
-    DDRD = 0x00;
-    DDRB = 0xFF;
-    for (;;)
-        allumerDEL(AMBRE);
-    
+    DDRD |= _BV(DDD4); //OC1B as output
+    TCCR1A |= _BV(COM1B1);  //Clear OC1A/OC1B on compare match
+	TCCR1B |= _BV(WGM13) 	//mode 8, PWM, Phase and Frequency Correct (TOP value is ICR1)
+   		   |  _BV(CS11); 	//prescaler(8)
+   	const uint16_t note[] = { 1911, 1703, 1517 , 1432 , 1276 , 1136 , 1012 };
+   	OCR1B = 100;
+   	uint8_t count = 0;
+   	for(;;){
+		ICR1H = (note[count] >> 8); //first set the high byte
+		ICR1L = note[count];        //now the low byte
+		_delay_ms(2000);
+		count++;
+		count=count%7;
+	}
     return 0;
 }
