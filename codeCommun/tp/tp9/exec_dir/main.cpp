85e0d37e894753fbe0eff05979dfdd18d9abea2b
Simon Zhang <simon.zhang@polymtl.ca>
2019-03-15 20:47:04 -0400

    add final touches

--- codeCommun/tp/tp9/exec_dir/main.cpp
+++ codeCommun/tp/tp9/exec_dir/main.cpp
@@ -2,20 +2,24 @@
 #include "couleurDel.h"
 #include "notes.h"
 
+#define _SIZEOF(x) sizeof(x)/sizeof(x[0])
+
 int main() {
+	//PINS 5 and 3 for piezo
+	DDRD |= _BV(DDD2); 
     DDRD |= _BV(DDD4); //OC1B as output
     TCCR1A |= _BV(COM1B1);  //Clear OC1A/OC1B on compare match
 	TCCR1B |= _BV(WGM13) 	//mode 8, PWM, Phase and Frequency Correct (TOP value is ICR1)
    		   |  _BV(CS11); 	//prescaler(8)
    	// const uint16_t note[] = { 1911, 1703, 1517 , 1432 , 1276 , 1136 , 1012 };
    	OCR1B = 100;
-   	uint8_t count = 0//56;
+   	uint8_t count = 0;//56;
    	for(;;){
 		ICR1H = (note[count] >> 8); //first set the high byte
 		ICR1L = note[count];        //now the low byte
 		_delay_ms(2000);
 		count++;
-		count=count%116;
+		count=count%(_SIZEOF(note));
 		// if(count==0){
 			// count=56;
 		// }
