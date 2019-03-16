c4ec29329408fcd6f438f5535cac6e24043b708a
Simon Zhang <simon.zhang@polymtl.ca>
2019-03-15 21:01:23 -0400

    correct indent and add source

--- codeCommun/tp/tp9/exec_dir/main.cpp
+++ codeCommun/tp/tp9/exec_dir/main.cpp
@@ -7,7 +7,7 @@
 int main() {
 	//PINS 5 and 3 for piezo
 	DDRD |= _BV(DDD2); 
-    DDRD |= _BV(DDD4); //OC1B as output
+	DDRD |= _BV(DDD4); //OC1B as output
     TCCR1A |= _BV(COM1B1);  //Clear OC1A/OC1B on compare match
 	TCCR1B |= _BV(WGM13) 	//mode 8, PWM, Phase and Frequency Correct (TOP value is ICR1)
    		   |  _BV(CS11); 	//prescaler(8)
@@ -26,3 +26,6 @@ int main() {
 	}
     return 0;
 }
+
+//source:
+//https://web.archive.org/web/20111128004851/http://aquaticus.info/pwm-music
