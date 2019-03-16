08f08d437a1ac5ef1e7802c95a73a62fa4681fb4
Simon Zhang <simon.zhang@polymtl.ca>
2019-03-15 21:01:51 -0400

    correct another indent reee pls work

--- codeCommun/tp/tp9/exec_dir/main.cpp
+++ codeCommun/tp/tp9/exec_dir/main.cpp
@@ -8,7 +8,7 @@ int main() {
 	//PINS 5 and 3 for piezo
 	DDRD |= _BV(DDD2); 
 	DDRD |= _BV(DDD4); //OC1B as output
-    TCCR1A |= _BV(COM1B1);  //Clear OC1A/OC1B on compare match
+	TCCR1A |= _BV(COM1B1);  //Clear OC1A/OC1B on compare match
 	TCCR1B |= _BV(WGM13) 	//mode 8, PWM, Phase and Frequency Correct (TOP value is ICR1)
    		   |  _BV(CS11); 	//prescaler(8)
    	// const uint16_t note[] = { 1911, 1703, 1517 , 1432 , 1276 , 1136 , 1012 };
