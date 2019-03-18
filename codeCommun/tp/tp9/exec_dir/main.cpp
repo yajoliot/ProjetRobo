4c81020779d49ad47abfd33846a503874ee02b9e
Simon Zhang <simon.zhang@polymtl.ca>
2019-03-18 16:02:37 -0400

    add description and help notes

--- codeCommun/tp/tp9/exec_dir/main.cpp
+++ codeCommun/tp/tp9/exec_dir/main.cpp
@@ -8,6 +8,47 @@
 #include "memoire_24.h"
 #include "usart.h"
 
+//Setup is as such
+
+// Pwm/Piezo are the same as in the modules branch:
+
+// MOTOR PWM
+// PD2 + PD4 (identified as 1 and 5 on the board) for roueGauche
+// PD5 + PD7 (identified as 6 and 8 on the board) for roueDroite
+// PD2: direction 
+// PD4: pwm
+// PD5: pwm
+// PD7: direction
+// Plug these so that the direction pin is connected to the D pin on the pont H for either roue and the pwm pin to the E pin on either roue 
+
+// PIEZO PWM
+// PD3 or PD4 create the PWM (or... Pin(5) and Pin(6) as identified on the board)
+// Choose any of the surrounding pins to set to ground and pass it to the constructor along with the PD3 or PD4
+// Plug the piezo in any way but on those pins you've passed to the constructor
+
+// DEL LIGHT
+// (+) sign -> PINB0 (identified as 1 on the board)
+// (-) sign -> PINB1 (identified as 2 on the board)
+
+// CAVALIERS
+// - IntEN has to have a cavalier
+
+// POWER SUPPLY
+// - Motherboard: Can be supplied by USB. We are not dealing with CAN. So it's not that serious.
+// - Pont H: Needs preferably new AA batteries (6). If not try to use the power supply in the labs.
+
+// NOTES:
+// 1. Motor needs the knob to be pulled backwards (towards the Pont H) to be supplied by the batteries
+// 2. Clicking on the interrupt will switch the whole thing into the WRITE_MODE which is basically the mode to write the bytecode into the EEPROM
+//     2.1. You must reset/restart to get back to the READ_MODE
+// 3. Les fonctions DEBUG_... sont les fonctions de debogage. Elles permettent d'envoyer des messages de diagnostique a l'ordinateur branchee qui a serieViaUSB d'ouvert.
+//     3.1. Elles peuvent etre totalement enlevees du program en utilisant le script a la racine de ce projet "setmode.sh"
+//          3.1.1. Les deux arguments que ce script prend sont: "debug" et "release". Lorsque vous passez l'argument "debug", ce script ira changer des flags de compilation
+//                 que le Makefile utilise pour permettre aux fonction de debogage de faire sortir des messages. Sinon avec "release", les fonctions de debogages n'existeront
+//                 pas et elles en fait ne seront meme pas incluses dans le programme, ce qui rend le programme beaucoup moins lourd.
+// 4. Essayez de "make clean" toujours avant de faire un "make"/"make install"
+// 5. Demandez des questions
+
 const uint8_t WRITE_MODE = 0x00;
 const uint8_t READ_MODE = 0x01;
 //DEFAULT MODE is to READ
@@ -44,8 +85,12 @@ ISR (INT0_vect) {
 int main() {
 //Initialization
   cli();
+  //PORTS
+  DDRB = 0xFF;
+  //INTERRUPTS
   EICRA |= _BV(ISC00);
   EIMSK |= _BV(INT0);
+  //LIBRARY INITS
   Memoire24CXXX memoire;
   TransmissionUART uart;
   initPWM();
@@ -53,7 +98,6 @@ int main() {
   sei();
 
 //Initial Sequence
-  DDRB=0xFF;
   for(uint8_t i=0 ; i<3 ; i++){
     allumerDEL(VERT);
     _delay_ms(1000);
@@ -116,7 +160,7 @@ int main() {
           break;
 
         case 0x45 : // eteindre les DEL : det
-          PORTB &= 0xFC;
+          allumerDEL(ETEIND);
           break;
         
         case 0x48 : // jouer une sonorite : SGO  
