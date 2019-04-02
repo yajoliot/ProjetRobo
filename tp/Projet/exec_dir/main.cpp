e48eaa6bc465fecd9719b942eeab5efcc246b655
Simon Zhang <simon.zhang@polymtl.ca>
2019-04-02 15:28:22 -0400

    add unit test for leds

--- tp/Projet/exec_dir/main.cpp
+++ tp/Projet/exec_dir/main.cpp
@@ -1,62 +1,62 @@
-#ifndef F_CPU
- #define F_CPU 8000000UL
- #endif
+// #ifndef F_CPU
+//  #define F_CPU 8000000UL
+//  #endif
  
- #include <util/atomic.h>
- #include <util/delay.h>
- #include "pwm.h"
- #include "piezo.h"
- #include "usart.h"
- #include "memoire_24.h"
- #include "usart.h"
- #include "bytecode.h"
- #include "util.h"
- #include "linetracker.h"
- #include "debug.h"
+//  #include <util/atomic.h>
+//  #include <util/delay.h>
+//  #include "pwm.h"
+//  #include "piezo.h"
+//  #include "usart.h"
+//  #include "memoire_24.h"
+//  #include "usart.h"
+//  #include "bytecode.h"
+//  #include "util.h"
+//  #include "linetracker.h"
+//  #include "debug.h"
 
-int main() {
-    PWM pwm;
-    LineTracker lineTracker;
-    enum etats {LIGNE_DROITE, TOURNE_GAUCHE, ARRETE};
-    int etat = LIGNE_DROITE;
-    DDRC = 0xFF;
-    DDRB = 0xFF;
-    uint8_t rapport = 140; // à changer pas bon -xavier
+// int main() {
+//     PWM pwm;
+//     LineTracker lineTracker;
+//     enum etats {LIGNE_DROITE, TOURNE_GAUCHE, ARRETE};
+//     int etat = LIGNE_DROITE;
+//     DDRC = 0xFF;
+//     DDRB = 0xFF;
+//     uint8_t rapport = 140; // à changer pas bon -xavier
 
-    for(;;){
+//     for(;;){
         
-        lineTracker.updateValueMap();
-        uint8_t valueMap = lineTracker.getValueMap();
-        PORTC = valueMap;
+//         lineTracker.updateValueMap();
+//         uint8_t valueMap = lineTracker.getValueMap();
+//         PORTC = valueMap;
 
-        //TODO:  mettre dans une fonction
-        if( valueMap == 4 || valueMap == 6 ||valueMap == 12 || valueMap == 8 || valueMap == 2 || valueMap == 3 || valueMap == 24 ||valueMap == 1 || valueMap == 16)
-            etat = LIGNE_DROITE;
-        else if(valueMap == 7 || valueMap == 15 || valueMap == 1 || valueMap == 0)
-            etat = TOURNE_GAUCHE;
-        else 
-            etat = ARRETE;
+//         //TODO:  mettre dans une fonction
+//         if( valueMap == 4 || valueMap == 6 ||valueMap == 12 || valueMap == 8 || valueMap == 2 || valueMap == 3 || valueMap == 24 ||valueMap == 1 || valueMap == 16)
+//             etat = LIGNE_DROITE;
+//         else if(valueMap == 7 || valueMap == 15 || valueMap == 1 || valueMap == 0)
+//             etat = TOURNE_GAUCHE;
+//         else 
+//             etat = ARRETE;
         
-        //TODO:  mettre dans une fonction
-        switch(etat){
-            case LIGNE_DROITE:
-                    pwm.avancementAjuste(rapport, valueMap);
-                break;
+//         //TODO:  mettre dans une fonction
+//         switch(etat){
+//             case LIGNE_DROITE:
+//                     pwm.avancementAjuste(rapport, valueMap);
+//                 break;
             
-            case TOURNE_GAUCHE:
-                    pwm.tournantGauche(rapport, valueMap);
-                break;
+//             case TOURNE_GAUCHE:
+//                     pwm.tournantGauche(rapport, valueMap);
+//                 break;
             
-            case ARRETE:
-                    pwm.arreter();
-                break;
-        }
+//             case ARRETE:
+//                     pwm.arreter();
+//                 break;
+//         }
         
-    }
-}
+//     }
+// }
 
 
-/*These define lines can be in the util.h or the constantes.h file (probably constantes.h file)
+// These define lines can be in the util.h or the constantes.h file (probably constantes.h file)
 #define F_CPU 8000000UL
 #include <avr/io.h>
 #include <util/delay.h>
@@ -89,4 +89,4 @@ int main(){
     // 6. DEL3 && DEL1
     // and so on...
 }
-*/
+
