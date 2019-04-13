e50fdea2347e2a8e81fbf8b96b480cd2c1f360fc
Simon Zhang <simon.zhang@polymtl.ca>
2019-04-13 06:15:37 -0400

    SIRC SENDER WORKS

--- tp/Projet/exec_dir/main.cpp
+++ tp/Projet/exec_dir/main.cpp
@@ -71,12 +71,28 @@ void setupSIRC();
 void begin45msTimer();
 void end45msTimer();
 
+#define OVERFLOW_COUNT_TO_45_MS 0x3726 //14118 (in integer value)
+volatile uint16_t overflow_count = 0x0000;
+volatile uint8_t reach_end_45_ms = FALSE;
+
+ISR(TIMER2_OVF_vect){
+    overflow_count++;
+    if(overflow_count>=OVERFLOW_COUNT_TO_45_MS){
+        overflow_count = 0x0000;
+        end45msTimer();
+        reach_end_45_ms = TRUE;
+    }
+}
+
+
 //For one T worth of time -> 600 us / 25 us = 24 cycles
 #define CYCLES_PER_T 0x18 //24
 
 volatile uint8_t count = 0x00;
 volatile uint8_t reach_end_T = FALSE;
 
+
+//This ISR is to get a cycle done (T or 600 microseconds)
 ISR(TIMER1_OVF_vect)
 {
     //DEBUG_FUNCTION_CALL((uint8_t*)"ISR(TIMER1_OVF_vect)");
@@ -97,18 +113,7 @@ ISR(TIMER1_OVF_vect)
     }
 }
 
-#define OVERFLOW_COUNT_TO_45_MS 0x3726 //14118 (in integer value)
-volatile uint16_t overflow_count = 0x0000;
-volatile uint8_t reach_end_45_ms = FALSE;
-
-ISR(TIMER2_OVF_vect){
-    overflow_count++;
-    if(overflow_count>=OVERFLOW_COUNT_TO_45_MS){
-        overflow_count = 0x0000;
-        end45msTimer();
-        reach_end_45_ms = TRUE;
-    }
-}
+void transmitBit(uint8_t bit);
 
 int main() {
     DEBUG_INFO((uint8_t*)"START OF PROGRAM");
@@ -129,34 +134,62 @@ int main() {
 //Test function is below
 
     //dummy command
-    uint8_t command = 0x01;
+    // uint8_t command = 0x01;
     //dummy address
-    uint8_t address = 0x02; //this will get ignored if in MODE_1
+    // uint8_t address = 0x02; //this will get ignored if in MODE_1
 
     //Tested function
 
-    transmit(command, address);
+    // transmit(command, address);
+    // for(;;){
+        transmit(0x01, 0x00);
+    // }
     
     DEBUG_INFO((uint8_t*)"END OF PROGRAM");
 }
 
-void setupSIRC(){
+void openPin(){
+    TCCR1A |= _BV(COM1A0);
+}
 
+void closePin(){
+    TCCR1A &= ~(_BV(COM1A0));
+}
+
+void transmitBit(uint8_t bit){
+    openPin();
+    if(bit == 1){
+        enableSIRC();
+        _delay_us(1200);
+        disableSIRC();
+    }else/*bit == 0x01*/{
+        enableSIRC();
+        _delay_us(600);
+        disableSIRC();
+    }
+    closePin();
+    PORTD &= ~(_BV(PD5));
+    //maybe need to set OC1A to 0?
+    _delay_us(600);
+}
+
+void setupSIRC(){
     //Set the registers : CTC mode, Toggle on Compare, No prescaler
-    TCCR1A |= _BV(WGM12) | _BV(COM1A0);
+    // TCCR1A |= _BV(COM1A0);
+    TCCR1B |=  _BV(WGM12);
     //TCCR1B |= _BV(CS10);
     
     //Set the frequency : 38 kHz
     //The following value is what we will use to set the "one"
     OCR1A = 0x68;
-
 }
 
 void enableSIRC(){
+    TCNT1 = 0x0000;
     //Non 0 value for the prescaler enables the timer
     TCCR1B |= _BV(CS10);
     //Enable counter overflow interrupt 
-    TIMSK1 |= _BV(TOIE1); 
+    // TIMSK1 |= _BV(TOIE1); 
 }
 
 void disableSIRC(){
@@ -167,75 +200,41 @@ void disableSIRC(){
     // cli();
 }
 
-void transmitOneCycle(uint8_t mode){
-    // DEBUG_FUNCTION_CALL((uint8_t*)"transmitOneCycle()");
-    //Set the PWM
-    enableSIRC();
-    //Wait until ISR ends itself the 24 cycles for 1 T
-    while(!reach_end_T){}
-    reach_end_T = FALSE;
-
-    // uint8_t tmp = TCCR1B;
-    // DEBUG_PARAMETER_VALUE((uint8_t*)"TCCR1B",&tmp);
-    // DEBUG_FUNCTION_EXIT();
-}
-
-void transmitHighBit(){
-    //DEBUG_FUNCTION_CALL((uint8_t*)"transmitHighBit()");
-    //Two Cycles @ Hi
-    for(uint8_t i=0x00 ; i<0x02; i++){
-        transmitOneCycle(HIGH_MODE);
-    }
-    //One Cycle @ Lo
-    transmitOneCycle(LOW_MODE);
-}
-
-void transmitLowBit(){
-    //DEBUG_FUNCTION_CALL((uint8_t*)"transmitLowBit()");
-    //One Cycle @ Hi
-    transmitOneCycle(HIGH_MODE);
-    //One Cycle @ Lo
-    transmitOneCycle(LOW_MODE);
-}
-
-void transmitHeader(){
-    DEBUG_FUNCTION_CALL((uint8_t*)"transmitHeader()");
-    //Four Cycles @ Hi
-    for(uint8_t i=0x00 ; i<0x04 ; i++){
-        transmitOneCycle(HIGH_MODE);
-    }
-    //One Cycle @ Lo
-    transmitOneCycle(LOW_MODE);
-}
-
-
 void transmitBits(uint8_t command_, uint8_t size_){
     //DEBUG_FUNCTION_CALL((uint8_t*)"transmitBits(uint8_t command_, uint8_t size_)");
     for(uint8_t i=0x00 ; i<size_ ; i++){
         //if the bit @ the ith position is 1
-        uint8_t bitmask = _BV(i);
-        //DEBUG_PARAMETER_VALUE((uint8_t*)"_BV(i)", &bitmask);
-        if(command_ & bitmask){
+        // uint8_t bitmask = _BV(i);
+        // //DEBUG_PARAMETER_VALUE((uint8_t*)"_BV(i)", &bitmask);
+        if(command_ & _BV(i)){
             //then transmit a high bit
-            transmitHighBit();
+            transmitBit(1);
         }else{
-            transmitLowBit();
+            transmitBit(0);
         }
     }
 }
 
+void transmitHeader(){
+    openPin(); 
+    enableSIRC();
+    _delay_us(2400);
+    disableSIRC();
+    closePin();
+    PORTD &= ~(_BV(PD5));
+    _delay_us(600);
+}
+
 void transmitCommand(uint8_t command_){
-    DEBUG_FUNCTION_CALL((uint8_t*)"transmitCommand(uint8_t command_)");
     transmitBits(command_, COMMAND_SIZE);
 }
 
 void transmitAddress(uint8_t address_){
-    DEBUG_FUNCTION_CALL((uint8_t*)"transmitAddress(uint8_t address_)");
     transmitBits(address_, ADDRESS_SIZE);
 }
 
 void transmit(uint8_t command_, uint8_t address_){
-    DEBUG_FUNCTION_CALL((uint8_t*)"transmit()");
+    // DEBUG_FUNCTION_CALL((uint8_t*)"transmit()");
     for(uint8_t i=0x00 ; i<0x03 ; i++){
         //begin 45 ms timer 3
         begin45msTimer();
@@ -249,7 +248,7 @@ void transmit(uint8_t command_, uint8_t address_){
         reach_end_45_ms = FALSE;
         //send again. for loop seems appropriate here
     }
-    DEBUG_FUNCTION_EXIT();
+    // DEBUG_FUNCTION_EXIT();
 }
 
 void begin45msTimer(){
